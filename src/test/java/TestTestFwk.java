3a7206bb434f28b701e92952577a5a376c6dc89e
Emiliano Suárez <emilianosuarez@gmail.com>
2013-10-31 02:44:04 -0300

    Agrego TestTestFwk.java: clase que testea Test con nuestro FWK

---
+++ src/test/java/TestTestFwk.java
@@ -0,0 +1,64 @@
+package test.java;
+
+import main.java.Test;
+
+public class TestTestFwk extends Test {
+
+    public static void main(String[] args) throws Exception {
+		run();
+	}
+
+	public static void run()
+	{
+		equalsObjectTest();
+		notEqualsObjectTest();
+		notEqualsIntTest();
+		equalsIntTest();
+		equalsBooleanTest();
+		notEqualsBooleanTest();
+		equalsCharTest();
+		notEqualsCharTest();
+		equalsFloatTest();
+		notEqualsFloatTest();
+	}
+
+	public static void equalsObjectTest() {
+		assertEquals("equalsObjectTest", "Hello", "Hello");
+	}
+
+	public static void notEqualsObjectTest() {
+		assertNotEquals("notEqualsObjectTest", "Hello", "ByeBye");
+	}
+	
+	public static void notEqualsIntTest() {
+		assertNotEquals("notEqualsIntTest", 1, 2);
+	}
+
+	public static void equalsIntTest() {
+		assertEquals("equalsIntTest", 1, 1);
+	}
+
+	public static void equalsBooleanTest() {
+		assertEquals("equalsBooleanTest", true, true);
+	}
+
+	public static void notEqualsBooleanTest() {
+		assertNotEquals("notEqualsBooleanTest", true, false);
+	}
+
+	public static void equalsCharTest() {
+		assertEquals("equalsCharTest", 'a', 'a');
+	}
+
+	public static void notEqualsCharTest() {
+		assertNotEquals("notEqualsCharTest", 'a', 'b');
+	}
+	
+	public static void equalsFloatTest() {
+		assertEquals("equalsFloatTest", 1.1, 1.1);
+	}
+
+	public static void notEqualsFloatTest() {
+		assertNotEquals("notEqualsFloatTest", 1.1, 1.2);
+	}
+}
