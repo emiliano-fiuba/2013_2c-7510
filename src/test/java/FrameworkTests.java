2796049fe3b6a8739bc4c0c5e0207b384ff3002f
Leandro Miguenz <leandro.v.059@gmail.com>
2013-11-07 10:13:53 -0300

    Subo tests faltantes de Affirm y borró la clase con los tests en el formato viejo

--- src/test/java/FrameworkTests.java
+++ src/test/java/FrameworkTests.java
@@ -19,13 +19,25 @@ public class FrameworkTests {
 		NotEqualsObjectTest notEqualsObjectTest = new NotEqualsObjectTest("Test two objects are different");
 		NotEqualsIntTest notEqualsIntTest = new NotEqualsIntTest("Test two integers are different");
 		EqualsIntTest equalsIntTest = new EqualsIntTest("Test two integers are equal");
+		NotEqualsFloatTest notEqualsFloatTest = new NotEqualsFloatTest("Test two floats are different");
+		EqualsFloatTest equalsFloatTest = new EqualsFloatTest("Test two floats are equal");
+		NotEqualsBooleanTest notEqualsBooleanTest = new NotEqualsBooleanTest("Test two booleans are different");
+		EqualsBooleanTest equalsBooleanTest = new EqualsBooleanTest("Test two booleans are equal");
+		NotEqualsCharTest notEqualsCharTest = new NotEqualsCharTest("Test two chars are different");
+		EqualsCharTest equalsCharTest = new EqualsCharTest("Test two chars are equal");
 		
 		try {
 			testSuiteTest.addTest(equalsObjectTest);
 			testSuiteTest.addTest(notEqualsObjectTest);
 			testSuiteTest.addTest(notEqualsIntTest);
 			testSuiteTest.addTest(equalsIntTest);
-			//Hqy que generar los tests de Affirm y agregarlos al suite, luego agregar el suite a testSuiteAll
+			testSuiteTest.addTest(notEqualsFloatTest);
+			testSuiteTest.addTest(equalsFloatTest);
+			testSuiteTest.addTest(notEqualsBooleanTest);
+			testSuiteTest.addTest(equalsBooleanTest);
+			testSuiteTest.addTest(notEqualsCharTest);
+			testSuiteTest.addTest(equalsCharTest);
+			
 			testSuiteAll.addTest(testSuiteTest);
 			
 		}catch (TestExistsException e) {
